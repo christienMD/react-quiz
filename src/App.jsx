0c46b6ee582e387a434d591c91e1f75c75853d23
Md Christien <mdchristien12@gmail.com>
2024-10-04 14:50:20 +0100

    setup json server , build reducer function and build the start screen components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,51 @@
+import { useEffect, useReducer } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
+
+const initialState = {
+  questions: [],
+
+  // loading , error , ready , active , finished ,
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.payload, status: "ready" };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error(" Action is unknown ");
+  }
+}
 
 const App = () => {
+  const [{ status, questions }, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) =>
+        res
+          .json()
+          .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      )
+      .catch((err) => dispatch({ type: "dataFailure" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      
+
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen />}
       </Main>
     </div>
   );
