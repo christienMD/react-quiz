7aa057c40417a21961a55e2ccd676614934e34ab
Md Christien <mdchristien12@gmail.com>
2024-10-04 13:58:10 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
+};
+
+export default App;
